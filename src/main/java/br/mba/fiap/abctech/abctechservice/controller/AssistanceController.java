ba91b54c3c786d2c46c8e709752e320d75c251b4
rafaeuz <alfaer@DESKTOP-BU5Q6TF.localdomain>
2022-04-24 04:17:34 -0300

    teste cross origin no AssistanceController

--- src/main/java/br/mba/fiap/abctech/abctechservice/controller/AssistanceController.java
+++ src/main/java/br/mba/fiap/abctech/abctechservice/controller/AssistanceController.java
@@ -4,12 +4,14 @@ import br.mba.fiap.abctech.abctechservice.application.AssistanceApplication;
 import br.mba.fiap.abctech.abctechservice.application.dto.AssistDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/assistance")
 public class AssistanceController {
